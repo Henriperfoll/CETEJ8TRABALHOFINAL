206a2a2a5aaea497a85cd675e7fe60dc1f7244ad
Henrique Perfoll Neto <hperfoll@gmail.com>
2019-09-14 10:55:33 -0300

    add JacksonJsonProvider

--- src/main/java/henriperfoll/cetej8trabalhofinal/DAO/DAOCidade.java
+++ src/main/java/henriperfoll/cetej8trabalhofinal/DAO/DAOCidade.java
@@ -5,10 +5,12 @@
  */
 package henriperfoll.cetej8trabalhofinal.DAO;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import henriperfoll.cetej8trabalhofinal.entity.Cidade;
 import java.util.ArrayList;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -18,20 +20,24 @@ import javax.ws.rs.core.MediaType;
 public class DAOCidade {
 
     public ArrayList<Cidade> carregaCidades() {
-        Client client = Client.create();
+        DefaultClientConfig defaultClientConfig = new DefaultClientConfig();
+        defaultClientConfig.getClasses().add(JacksonJsonProvider.class);
+        Client client = Client.create(defaultClientConfig);
         WebResource resource = client.resource("http://maventest.herokuapp.com/mavenTest-1.0-SNAPSHOT/webresources/cidade");
         resource.type(MediaType.APPLICATION_JSON);
         Cidade[] cidades = resource.get(Cidade[].class);
-        ArrayList<Cidade> listaCidades = new ArrayList<>();
-        for(Cidade c : cidades){
+        ArrayList<Cidade> listaCidades = new ArrayList();
+        for (Cidade c : cidades) {
             listaCidades.add(c);
         }
         return listaCidades;
     }
-    
-    public Cidade carregaCidade(long id){
-        Client client = Client.create();
-        WebResource resource = client.resource("http://maventest.herokuapp.com/mavenTest-1.0-SNAPSHOT/webresources/cidade/"+id);
+
+    public Cidade carregaCidade(long id) {
+        DefaultClientConfig defaultClientConfig = new DefaultClientConfig();
+        defaultClientConfig.getClasses().add(JacksonJsonProvider.class);
+        Client client = Client.create(defaultClientConfig);
+        WebResource resource = client.resource("http://maventest.herokuapp.com/mavenTest-1.0-SNAPSHOT/webresources/cidade/" + id);
         resource.type(MediaType.APPLICATION_JSON);
         return resource.get(Cidade.class);
     }
